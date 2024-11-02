f47f9fcbba0abf825a93fcbbab1b3a2ba07e6559
andreyka101 <andrejkvasnin236@gmail.com>
2024-11-02 12:24:03 +0300

    first commit

---
+++ lesson_1.py
@@ -0,0 +1,61 @@
+# print("hello world")
+# print('hello red')
+# print(20)
+
+
+
+# num = 4
+# print(num)
+
+
+
+# print(7.4)
+# print(7 , 4 , "eyfgefy" , "ehhehe" , num)
+
+
+
+
+# print("3 + 2 =" , 3 + 2)
+# print("3 - 2 =" , 3 - 2)
+# print("3 * 3 =" , 3 * 3)
+# print("3 ** 3 =" , 3 ** 3)
+# print("10 / 3 =" , 10 / 3)
+
+
+
+# red_car = 0
+# redCarSpeed = 0
+# q1wer4ty9 = 3
+
+
+
+# num_2 = 6
+# print(num_2)
+# num_2 = 4
+# print(num_2)
+
+# num_2 = num_2 + 8
+# print(num_2)
+
+
+
+# num_int = 8
+# num_float = 4.2
+# num_str = "qwerty"
+# num_bool = False
+# num_null = None
+
+# print("num_int =" , num_int)
+# print("num_float =" , num_float)
+# print("num_str =" , num_str)
+# print("num_bool =" , num_bool)
+# print("num_null =" , num_null)
+
+
+
+# num_3 = None
+# print(num_3)
+# num_3 = "hello"
+# print(num_3)
+
+# 8 952 857 69 44
